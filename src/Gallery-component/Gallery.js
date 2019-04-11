d1dcef4bf0db04329d64ae9169831dfbb281b705
Kinga Wrona <kingawr123@gmail.com>
2019-04-11 17:01:43 +0200

    no message

--- src/Gallery-component/Gallery.js
+++ src/Gallery-component/Gallery.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './Gallery.css';
-import { MyImage } from '../About-me-component/MyImg';
 
-export class Galery extends React.Component{
-    render(){
-        return(
-            <div className="gallery-container">
-                <MyImage containerClass="img-in-gallery-cont" imgClass="img-in-gallery"/>
+export class Gallery extends React.Component {
+    render() {
+        return (
+            <div>
+
             </div>
         )
     }
